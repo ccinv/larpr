a9eafbb882e49416b94488e84600aaa42bb5403d
avaicode <avaicode@gmail.com>
2017-03-02 13:29:41 +0800

    Fix: Expectant fixing

--- container.c
+++ container.c
@@ -15,8 +15,9 @@ int main(int argc, char* argv[]) {
     luaL_openlibs(L);
     lua_newtable(L);
     for (i = 0; i < argc; ++i) {
+        lua_pushnumber(L, i);
         lua_pushstring(L, argv[i]);
-        lua_seti(L, -2, i);
+		lua_rawset(L, -3);
     }
     larpr_init(L, argv[0]);
     lua_close(L);
