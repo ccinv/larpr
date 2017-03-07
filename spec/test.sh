5b4293cdb97bcd953aa0565a868854403c511470
avaicode <749881399@qq.com>
2017-03-07 21:23:10 +0800

    Fix: Given up it(dann)

--- spec/test.sh
+++ spec/test.sh
@@ -1,4 +1,4 @@
-./tools/lar.moon -b -s ./spec/testcases testcases.lar
+moon ./tools/lar.moon -b -s ./spec/testcases testcases.lar
 touch raw
-./tools/freeze.moon raw testcases container testcases.lar
+moon ./tools/freeze.moon raw testcases container testcases.lar
 busted
