6fff0c85d34862189a76a5b917ff1e90e03ca76d
avaicode <749881399@qq.com>
2017-03-12 20:23:57 +0800

    Improve: Lua instead of shell script

--- spec/test.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cmd /c moon .\tools\lar.moon -b -s .\spec\testcases testcases.lar
-copy nul raw
-cmd /c moon .\tools\freeze.moon raw testcases container testcases.lar
-cmd /c busted
--- spec/test.sh
+++
@@ -1,4 +0,0 @@
-moon ./tools/lar.moon -b -s ./spec/testcases testcases.lar
-touch raw
-moon ./tools/freeze.moon raw testcases container testcases.lar
-busted
