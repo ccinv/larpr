a4c4b2fa5d1f5c33540cced3af3693f5c8e4db6a
avaicode <749881399@qq.com>
2017-03-12 20:27:24 +0800

    Fix: Lua instead of shell script

---
+++ spec/dotest.lua
@@ -0,0 +1,22 @@
+-- The MIT License (MIT)
+-- Copyright (c) 2016 Yanghe Sun
+
+local info = require("path.info")
+
+if info.platform == "windows" then
+    local cmd = ""
+    cmd = cmd .. [[cmd /c moon .\tools\lar.moon -b -s .\spec\testcases testcases.lar&&]]
+    cmd = cmd .. [[copy nul raw > nul&&]]
+    cmd = cmd .. [[cmd /c moon .\tools\freeze.moon raw testcases container testcases.lar&&]]
+    cmd = cmd .. [[cmd /c busted]]
+    os.execute(cmd)
+end
+
+if info.platform == "linux" then
+    local cmd = ""
+    cmd = cmd .. [[moon ./tools/lar.moon -b -s ./spec/testcases testcases.lar&&]]
+    cmd = cmd .. [[touch raw&&]]
+    cmd = cmd .. [[moon ./tools/freeze.moon raw testcases container testcases.lar&&]]
+    cmd = cmd .. [[busted]]
+    os.execute(cmd)
+end
